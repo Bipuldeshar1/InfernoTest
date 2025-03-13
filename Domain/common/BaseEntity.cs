a47a7be8e024e04cd8672c266a6343c5dea6f8c9
Bipul Deshar <bipul.deshar111@gmail.com>
2025-03-13 14:37:07 +0545

    Add project files.

---
+++ Domain/common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.common
+{
+    internal class BaseEntity
+    {
+        public string ExposeId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
